3f88c4416a54afc3a27278826e734fa0b4d96eb6
Akhilesh Jaiswal <nietakhilesh@gmail.com>
2022-01-17 20:13:37 -0800

    demo 1 by ak

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+// new comment by ak
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
