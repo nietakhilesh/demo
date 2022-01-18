c8bf47727c10c145fd10c0491d428bc341de84e6
hs3773 <hs3773@nyu.edu>
2022-01-17 20:14:53 -0800

    creating conflict

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 // demo push new branch
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// New commit to demo conflict
 @SpringBootApplication
 public class DemoApplication {
 
